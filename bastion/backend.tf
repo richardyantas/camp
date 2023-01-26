c7b8a00e24cfd89623d273fc99a994aa567729b7
Richard Yantas <richard@dysopsis.com>
2023-01-25 22:06:27 -0500

    terr4

--- bastion/backend.tf
+++ bastion/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket          = "tfstate-bucket-4291-ideasextraordinarias-default"
+    # bucket          = "tfstate-bucket-4291-ideasextraordinarias-default"
     prefix          = "bastion/terraform.tfstate"
   }
 }
